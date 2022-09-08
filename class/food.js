d2838272067a0649767dbb0240bef4bc501a07b0
TLTechbender <myjoyhasarrived2004@gmail.com>
2022-09-08 18:43:30 +0100

    feat: create food class and was confused

--- class/food.js
+++ class/food.js
@@ -1,8 +1,12 @@
+const Item = require("./item");
 
 // FILL THIS OUT
-
-class Food {
-
+/* I'm finding this difficult so far, this problem set is different from the ones I've been encountering so far, as usual the mocha test ain't working, funny enough this time around I'm finding the spec file difficult to read*/
+class Food extends Item {
+    constructor(name,description){
+        this.name=name;
+        this.description=description;
+    }
 }
 
 module.exports = {
